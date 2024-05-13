47e64a4b841a73068639a97788a8191cc5a49873
Gubochka <newicraft2201@gmail.com>
2024-05-14 00:41:14 +0300

    Created plugin to integrate mod recipes to JEI

---
+++ src/main/java/org/reims/industrial_integration/integration/AbstractMachineRecipeCategory.java
@@ -0,0 +1,76 @@
+package org.reims.industrial_integration.integration;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.mojang.blaze3d.vertex.PoseStack;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.drawable.IDrawableAnimated;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import org.reims.industrial_integration.block.ModBlocks;
+import org.reims.industrial_integration.gui.utils.MachineInterfaceData;
+import org.reims.industrial_integration.gui.utils.Progressbar;
+import org.reims.industrial_integration.recipe.AbstractMachineRecipe;
+
+
+public abstract class AbstractMachineRecipeCategory<R extends AbstractMachineRecipe> implements IRecipeCategory<R> {
+    public static MachineInterfaceData machineData;
+    private final ResourceLocation TEXTURE;
+
+    private final IDrawable background;
+    private final IDrawable icon;
+    private final LoadingCache<Integer, IDrawableAnimated> cachedArrows;
+    protected final int offset = 4;
+
+    public AbstractMachineRecipeCategory(IGuiHelper helper) {
+        TEXTURE = machineData.BACKGROUND;
+        this.background = helper.createDrawable(TEXTURE, offset, offset, machineData.backgroundWidth-(offset*2), 85-offset);
+        this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.STATION_BLOCK.get()));
+        this.cachedArrows = CacheBuilder.newBuilder().build(new CacheLoader<>() {
+            @Override
+            public IDrawableAnimated load(Integer cookTime) {
+                Progressbar progressbar = machineData.progressbar;
+                IDrawableAnimated.StartDirection direction = IDrawableAnimated.StartDirection.LEFT;
+                if (progressbar.direction == Progressbar.AnimationDirection.Down) {
+                    direction = IDrawableAnimated.StartDirection.TOP;
+                }
+
+                return helper.drawableBuilder(TEXTURE, progressbar.offsetX, progressbar.offsetY, progressbar.width, progressbar.height)
+                        .buildAnimated(cookTime, direction, false);
+            }
+        });
+    }
+
+    @Override
+    public Component getTitle() {
+        return Component.literal(machineData.displayedName);
+    }
+
+    @Override
+    public IDrawable getBackground() {
+        return this.background;
+    }
+
+    @Override
+    public IDrawable getIcon() {
+        return this.icon;
+    }
+
+    protected IDrawableAnimated getProgressbar() {
+        return this.cachedArrows.getUnchecked(78); // 78 animation speed
+    }
+
+    @Override
+    public void draw(R recipe, IRecipeSlotsView recipeSlotsView, PoseStack stack, double mouseX, double mouseY) {
+        Progressbar progressbarData = machineData.progressbar;
+        IDrawableAnimated progressbar = getProgressbar();
+        progressbar.draw(stack, progressbarData.startX-offset, progressbarData.startY-offset);
+    }
+}
+
---
+++ src/main/java/org/reims/industrial_integration/integration/JEIIIPlugin.java
@@ -0,0 +1,55 @@
+package org.reims.industrial_integration.integration;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.RecipeManager;
+import org.reims.industrial_integration.Industrial_Integration;
+import org.reims.industrial_integration.gui.utils.MachineInterfaces;
+import org.reims.industrial_integration.recipe.CompressorRecipe;
+import org.reims.industrial_integration.recipe.StationRecipe;
+
+import java.util.List;
+import java.util.Objects;
+
+@JeiPlugin
+public class JEIIIPlugin implements IModPlugin {
+    public static RecipeType<StationRecipe> STATION_TYPE =
+            new RecipeType<>(StationRecipeCategory.UID, StationRecipe.class);
+
+    public static RecipeType<CompressorRecipe> COMPRESSOR_TYPE =
+            new RecipeType<>(getUID(MachineInterfaces.COMPRESSOR.UID), CompressorRecipe.class);
+
+    @Override
+    public ResourceLocation getPluginUid() {
+        return new ResourceLocation(Industrial_Integration.MODID, "jei_plugin");
+    }
+
+    private static ResourceLocation getUID(String id) {
+        return new ResourceLocation(Industrial_Integration.MODID, id);
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        registration.addRecipeCategories(new
+                StationRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
+
+        registration.addRecipeCategories(new
+                CompressorRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        RecipeManager rm = Objects.requireNonNull(Minecraft.getInstance().level).getRecipeManager();
+
+        List<StationRecipe> stationRecipesInfusing = rm.getAllRecipesFor(StationRecipe.Type.INSTANCE);
+        registration.addRecipes(STATION_TYPE, stationRecipesInfusing);
+
+        List<CompressorRecipe> compressorRecipesInfusing = rm.getAllRecipesFor(CompressorRecipe.Type.INSTANCE);
+        registration.addRecipes(COMPRESSOR_TYPE, compressorRecipesInfusing);
+    }
+}
