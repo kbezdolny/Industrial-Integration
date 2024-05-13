dc73f5b37549dad35aa6f372e838a1d921e22056
Gubochka <newicraft2201@gmail.com>
2024-05-14 00:19:45 +0300

    Added item registry and new custom items

---
+++ src/main/java/org/reims/industrial_integration/item/ModItems.java
@@ -0,0 +1,42 @@
+package org.reims.industrial_integration.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.reims.industrial_integration.Industrial_Integration;
+
+import static org.reims.industrial_integration.Industrial_Integration.MODID;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+    public static final RegistryObject<Item> INCOMPLETE_RAW_RUBBER = ITEMS.register("incomplete_raw_rubber",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_RUBBER = ITEMS.register("raw_rubber",
+            () -> new Item(new Item.Properties().tab(Industrial_Integration.TAB)));
+    public static final RegistryObject<Item> SAWDUST = ITEMS.register("sawdust",
+            () -> new Item(new Item.Properties().tab(Industrial_Integration.TAB)));
+
+
+    public static final RegistryObject<Item> CAPACITOR = ITEMS.register("capacitor",
+            () -> new Item(new Item.Properties().tab(Industrial_Integration.TAB)));
+    public static final RegistryObject<Item> DIODE = ITEMS.register("diode",
+            () -> new Item(new Item.Properties().tab(Industrial_Integration.TAB)));
+    public static final RegistryObject<Item> INDUCTOR = ITEMS.register("inductor",
+            () -> new Item(new Item.Properties().tab(Industrial_Integration.TAB)));
+    public static final RegistryObject<Item> RESISTOR = ITEMS.register("resistor",
+            () -> new Item(new Item.Properties().tab(Industrial_Integration.TAB)));
+    public static final RegistryObject<Item> TRANSISTOR = ITEMS.register("transistor",
+            () -> new Item(new Item.Properties().tab(Industrial_Integration.TAB)));
+    public static final RegistryObject<Item> ELECTRONIC_CIRCUIT = ITEMS.register("electronic_circuit",
+            () -> new Item(new Item.Properties().tab(Industrial_Integration.TAB)));
+    public static final RegistryObject<Item> IRON_PLATE = ITEMS.register("iron_plate",
+            () -> new Item(new Item.Properties().tab(Industrial_Integration.TAB)));
+
+}
