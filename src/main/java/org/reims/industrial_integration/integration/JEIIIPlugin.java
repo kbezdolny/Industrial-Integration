403fdca8547be1302f0524b911875c4adc3ad88b
Gubochka <newicraft2201@gmail.com>
2024-05-19 16:51:27 +0300

    Improved JEI integration: Added GuiHandler and RecipeCatalysts for recipes

--- src/main/java/org/reims/industrial_integration/integration/JEIIIPlugin.java
+++ src/main/java/org/reims/industrial_integration/integration/JEIIIPlugin.java
@@ -3,13 +3,20 @@ package org.reims.industrial_integration.integration;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeManager;
 import org.reims.industrial_integration.Industrial_Integration;
+import org.reims.industrial_integration.block.ModBlocks;
+import org.reims.industrial_integration.gui.screen.CompressorScreen;
+import org.reims.industrial_integration.gui.screen.SlicerScreen;
 import org.reims.industrial_integration.gui.utils.MachineInterfaces;
+import org.reims.industrial_integration.gui.utils.Progressbar;
 import org.reims.industrial_integration.recipe.CompressorRecipe;
 import org.reims.industrial_integration.recipe.SlicerRecipe;
 import org.reims.industrial_integration.recipe.StationRecipe;
@@ -61,5 +68,22 @@ public class JEIIIPlugin implements IModPlugin {
 
         List<SlicerRecipe> slicerRecipesInfusing = rm.getAllRecipesFor(SlicerRecipe.Type.INSTANCE);
         registration.addRecipes(SLICER_TYPE, slicerRecipesInfusing);
+    }
+
+    @Override
+    public void registerGuiHandlers(IGuiHandlerRegistration registration) {
+        Progressbar compressor = MachineInterfaces.COMPRESSOR.progressbar;
+        registration.addRecipeClickArea(CompressorScreen.class, compressor.startX, compressor.startY,
+                compressor.width, compressor.height, JEIIIPlugin.COMPRESSOR_TYPE);
+
+        Progressbar slicer = MachineInterfaces.SLICER.progressbar;
+        registration.addRecipeClickArea(SlicerScreen.class, slicer.startX, slicer.startY,
+                slicer.width, slicer.height, JEIIIPlugin.SLICER_TYPE);
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(ModBlocks.COMPRESSOR_BLOCK.get()), JEIIIPlugin.COMPRESSOR_TYPE);
+        registration.addRecipeCatalyst(new ItemStack(ModBlocks.SLICER_BLOCK.get()), JEIIIPlugin.SLICER_TYPE);
     }
 }
